48c14e2611d160633494d6e1353baf5630c5b6e2
Yakari144 <yakari144@DESKTOP-MANKC29.localdomain>
2024-08-06 23:49:28 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// Handle CORS related issues that might occur when trying to access the API from different domains during development and testing
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', 'http://stepwisesource.epl.di.uminho.pt');
+   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   next();
+ });
 app.use(cors())
 app.use('/', indexRouter);
 
